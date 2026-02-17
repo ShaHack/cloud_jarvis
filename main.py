ac463e8fdc4222cb5c21290fcca057c4aa2d8c08
ShaHack <sgokul.network@gmail.com>
2026-02-17 12:03:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import os
+import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-# get token from Railway
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large"
+headers = {"Authorization": f"Bearer {HF_TOKEN}"}
+
+
+def ask_ai(text):
+    payload = {"inputs": text}
+    response = requests.post(API_URL, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        return response.json()[0]["generated_text"]
+    else:
+        return "Try again later."
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Jarvis is online 🚀")
+    await update.message.reply_text("Jarvis AI ready")
 
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_text = update.message.text
-    await update.message.reply_text(f"You said: {user_text}")
+    answer = ask_ai(user_text)
+    await update.message.reply_text(answer)
 
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
-
-    print("Bot started...")
+    app.add_handler(MessageHandler(filters.TEXT, reply))
     app.run_polling()
 
 
